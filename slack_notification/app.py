cf11a0844882cba29b88443be371f166128f1ab5
Sung Hwang <sung.hwang@ubc.ca>
2025-07-16 13:30:09 -0700

    Added a function for processing the processed CD2 table data and outputs the human readable message.

--- slack_notification/app.py
+++ slack_notification/app.py
@@ -41,10 +41,31 @@ def send_to_slack(message):
     except Exception as e:
         logger.exception(f"An error occured during the send_to_slack() operation: {e}")
 
+def process_table_update_message(message):
+
+    # Merge all single-key dictionaries into one.
+    merged_data = {k: v for d in message for k, v in d.items()}
+
+    message = (
+        f'failed: {merged_data["failed"]} \n'
+        f'failed_init: {merged_data["failed_init"]} \n'
+        f'failed_sync: {merged_data["failed_sync"]} \n'
+        f'complete_with_update: {merged_data["complete_with_update"]} \n'
+        f'complete: {merged_data["complete"]} \n'
+        f'Number of Tables Failed : {merged_data["num_failed"]} \n'
+        f'Number of Tables Complete with Update : {merged_data["num_complete_with_update"]} \n'
+        f'Number of Complete: {merged_data["num_complete"]} \n'
+    )
+
+    return message
+
 def lambda_handler(event, context):
 
     # Get the SNS message payload
     sns_message = event['Records'][0]['Sns']['Message']
+
+    sns_message = process_table_update_message(sns_message)
+
     sns_title = "*Canvas Data 2 Workflow Notification*:\n"
     sns_message = sns_title + sns_message
 
